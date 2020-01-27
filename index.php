926f1bddeb14373f88c9cc5cbcfcd34bbdeb3c10
Aire <airidasams@gmail.com>
2020-01-27 23:04:13 +0200

    Initial commit

---
+++ index.php
@@ -0,0 +1,50 @@
+<?php
+
+require_once './vendor/autoload.php';
+
+use Symfony\Component\EventDispatcher\EventDispatcher;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\RequestStack;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Controller\ArgumentResolver;
+use Symfony\Component\HttpKernel\Controller\ControllerResolver;
+use Symfony\Component\HttpKernel\EventListener\RouterListener;
+use Symfony\Component\HttpKernel\HttpKernel;
+use Symfony\Component\Routing\Matcher\UrlMatcher;
+use Symfony\Component\Routing\RequestContext;
+use Symfony\Component\Routing\Route;
+use Symfony\Component\Routing\RouteCollection;
+
+$routes = new RouteCollection();
+$routes->add(
+    'hello',
+    new Route('/hello/{name}',
+        [
+            '_controller' => function (Request $request) {
+                return new Response('Hello ' . $request->get('name'));
+            }
+        ]
+    )
+);
+
+$request = Request::createFromGlobals();
+
+$matcher = new UrlMatcher($routes, new RequestContext());
+
+$dispatcher = new EventDispatcher();
+$dispatcher->addSubscriber(new RouterListener($matcher, new RequestStack()));
+
+$controllerResolver = new ControllerResolver();
+$argumentResolver = new ArgumentResolver();
+
+$kernel = new HttpKernel($dispatcher, $controllerResolver, new RequestStack(), $argumentResolver);
+
+try {
+    $response = $kernel->handle($request);
+} catch (Exception $e) {
+    dump($e);
+}
+
+$response->send();
+
+$kernel->terminate($request, $response);
