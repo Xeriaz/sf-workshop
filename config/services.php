8cacaefc49c4ede35c2689a9046f2c339308f8b8
Aire <airidasams@gmail.com>
2020-02-11 23:28:48 +0200

    Add services.php, use diLoader to load it

---
+++ config/services.php
@@ -0,0 +1,16 @@
+<?php
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+
+return function (ContainerConfigurator $configurator) {
+    $services = $configurator->services()
+        ->defaults()
+            ->autowire()
+            ->autoconfigure()
+    ;
+
+    $services->load('App\\', '../src/*')
+        ->exclude('../src/{DependencyInjection,Entity,Migrations,Tests,Kernel.php}');
+};
--- public/index.php
+++ public/index.php
@@ -3,27 +3,36 @@
 require_once __DIR__ . '/../vendor/autoload.php';
 
 use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader\PhpFileLoader;
 use Symfony\Component\EventDispatcher\EventDispatcher;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\RequestStack;
 use Symfony\Component\HttpKernel\Controller\ArgumentResolver;
-use Symfony\Component\HttpKernel\Controller\ControllerResolver;
+use Symfony\Component\HttpKernel\Controller\ContainerControllerResolver;
 use Symfony\Component\HttpKernel\EventListener\RouterListener;
 use Symfony\Component\HttpKernel\HttpKernel;
-use Symfony\Component\Routing\Loader\PhpFileLoader;
+use Symfony\Component\Routing\Loader\PhpFileLoader as RoutingLoader;
 use Symfony\Component\Routing\Matcher\UrlMatcher;
 use Symfony\Component\Routing\RequestContext;
 
-$phpFileLoader = new PhpFileLoader(new FileLocator());
+$fileLocator = new FileLocator();
+$containerBuilder = new ContainerBuilder();
+
+$loader = new PhpFileLoader($containerBuilder, $fileLocator);
+$loader->load(__DIR__ . '/../config/services.php');
+$containerBuilder->compile();
+
+$routingLoader = new RoutingLoader($fileLocator);
 
 $request = Request::createFromGlobals();
 
-$matcher = new UrlMatcher($phpFileLoader->load(__DIR__ . '/../config/routes.php'), new RequestContext());
+$matcher = new UrlMatcher($routingLoader->load(__DIR__ . '/../config/routes.php'), new RequestContext());
 
 $dispatcher = new EventDispatcher();
 $dispatcher->addSubscriber(new RouterListener($matcher, new RequestStack()));
 
-$controllerResolver = new ControllerResolver();
+$controllerResolver = new ContainerControllerResolver($containerBuilder);
 $argumentResolver = new ArgumentResolver();
 
 $kernel = new HttpKernel($dispatcher, $controllerResolver, new RequestStack(), $argumentResolver);
@@ -31,7 +40,7 @@ $kernel = new HttpKernel($dispatcher, $controllerResolver, new RequestStack(), $
 try {
     $response = $kernel->handle($request);
 } catch (Throwable $e) {
-    dump($e, $e->getTraceAsString());
+    dump($e);
     exit;
 }
 
