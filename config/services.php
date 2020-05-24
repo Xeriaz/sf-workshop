9f69862ee45413b1a8b847577fb165540dc3ed41
Aire <airidasams@gmail.com>
2020-05-24 23:29:29 +0300

    Register form services

--- config/services.php
+++ config/services.php
@@ -12,6 +12,10 @@ use Symfony\Component\DependencyInjection\Loader\PhpFileLoader;
 use Symfony\Component\DependencyInjection\Reference;
 use Symfony\Component\EventDispatcher\DependencyInjection\RegisterListenersPass;
 use Symfony\Component\EventDispatcher\EventDispatcher;
+use Symfony\Component\Form\Extension\HttpFoundation\HttpFoundationExtension;
+use Symfony\Component\Form\FormFactory;
+use Symfony\Component\Form\FormRegistry;
+use Symfony\Component\Form\ResolvedFormTypeFactory;
 use Symfony\Component\HttpFoundation\RequestStack;
 use Symfony\Component\HttpKernel\Controller\ArgumentResolver;
 use Symfony\Component\HttpKernel\Controller\ContainerControllerResolver;
@@ -26,6 +30,7 @@ return function (ContainerConfigurator $configurator, ContainerBuilder $containe
     $services = $configurator->services()
         ->defaults()
         ->bind('$greeter', ref('xeriaz.greeter.service'))
+        ->bind('$formFactory', ref('form_factory'))
         ->autowire()
         ->autoconfigure();
 
@@ -66,7 +71,7 @@ return function (ContainerConfigurator $configurator, ContainerBuilder $containe
         ->args(
             [
                 ref('twig.filesystem_loader'),
-                ['cache' => $container->getParameter('kernel.cache_dir')],
+//                ['cache' => $container->getParameter('kernel.cache_dir')], // Avoid twig caching
             ]
         );
     $services->set('greeter.command', GreeterCommand::class)
@@ -74,6 +79,25 @@ return function (ContainerConfigurator $configurator, ContainerBuilder $containe
             ref(GreeterService::class)
         ]);
     $services->set('logger', Logger::class);
+
+    // FORM
+    $services->set('http_foundation.extension', HttpFoundationExtension::class);
+    $services->set('resolved_form_type.factory', ResolvedFormTypeFactory::class);
+    $services->set('form_registry', FormRegistry::class)
+        ->args(
+            [
+                [
+                    ref('http_foundation.extension'),
+                ],
+                ref('resolved_form_type.factory'),
+            ]
+        );
+    $services->set('form_factory', FormFactory::class)
+        ->args(
+            [
+                ref('form_registry'),
+            ]
+        );
 
     $container->addCompilerPass(new RegisterListenersPass());
     $container->register(ViewListener::class)
